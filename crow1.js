a98d997cb45417621aa3cd26a10f87e9b00f3ae6
marksmanship021 <black.sword021@gmail.com>
2024-03-18 18:51:03 +0200

    first change

---
+++ crow1.js
@@ -0,0 +1,22 @@
+// - створити блок,
+// - додати йому класи wrap, collapse, alpha, beta
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// - додати цей блок в body.
+// - клонувати його повністю, та додати клон в body.
+
+//document.createElement
+//classList.add
+//appendChild
+//cloneNode
+
+const block = document.createElement('div');
+
+block.classList.add(wrap, collapse, alpha, beta)
+
+block.style.backgroundColor = 'lightblue';
+block.style.color = 'white';
+block.style.fontSize = '20px';
+
+document.body.appendChild(block); 
+
+let clonedBlock = block.cloneNode(true);
